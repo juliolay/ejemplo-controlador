c81eda0325b1e96c277e6131fbaee13baf387bee
juliolay <juliolayblanco@gmail.com>
2021-02-08 06:50:51 -0800

    actividad 2

---
+++ actividad 2 juio blanco21004466.js
@@ -0,0 +1,20 @@
+var mes=10
+mes=prompt("ingrese el mes")
+
+if(mes >= 1 && mes <= 2){
+alert("invierno");
+}
+else if(mes >= 12 && mes <= 13){
+  alert("invierno");
+}
+else if(mes >= 3 && mes <= 5){
+  alert("primavera");
+}
+else if(mes >= 6 && mes <= 8){
+  alert("verano");
+}
+else if(mes >= 9 && mes <= 11){
+  alert("otoño");
+}
+
+ alert("julio cesar blanco monroy 21004466")
